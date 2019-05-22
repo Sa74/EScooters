cce8bd6fe983f0764f8a69e73702cf5f1355abae
Sasi M <msasi7274@gmail.com>
2019-05-22 22:24:07 +0530

    - Initial commit after project creation

---
+++ EScooters/EScooters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EScooters
+//
+//  Created by Sasi Moorthy on 22/05/19.
+//  Copyright © 2019 Sasi Moorthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
