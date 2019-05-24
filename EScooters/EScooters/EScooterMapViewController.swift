c235f7164bdfabe00a38d59eefc7de73150607da
Sasi M <msasi7274@gmail.com>
2019-05-24 18:22:44 +0530

    - Centre map in Berlin area since vehicle data is present in those regions alone

--- EScooters/EScooters/EScooterMapViewController.swift
+++ EScooters/EScooters/EScooterMapViewController.swift
@@ -18,6 +18,11 @@ class EScooterMapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Center map in Berlin area since vehicle data is present in those regions alone
+        let coordinate = CLLocationCoordinate2D(latitude: 52.52000, longitude: 13.4050)
+        let mapRegion = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        eScooterMapView.setRegion(mapRegion, animated: true)
+        
         eScooterViewModel.updateHandler = { [weak self] in
             print("Reload")
         }
